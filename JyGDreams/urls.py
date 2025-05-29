96fd11b913c2fd4806a24b06813bfe7eed5e7663
GaboGon25 <gabrieltablet969@gmail.com>
2025-05-29 14:33:36 -0600

    Login Listo(Sujeto a revision)

--- JyGDreams/urls.py
+++ JyGDreams/urls.py
@@ -3,8 +3,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),
+    #path('', views.index, name='index'),
+    path('', views.login_view, name='login'),  # Ahora la raiz es el login para que se muestre primero
+    path('index/', views.index, name='index'),  # ojo aca
     path('agregar_paciente/', views.agregar_paciente, name='agregar_paciente'),
-     path('login/', views.login, name='login'),
+    path('login/', views.login_view, name='login'),
 
 ]
--- JyGDreams/views.py
+++ JyGDreams/views.py
@@ -1,7 +1,20 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login
+from django.contrib import messages
 from django.http import HttpResponse
 
-def login(request):
+#def login(request):
+    #return render(request, 'login.html')
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('index')  # Cambia 'home' por el nombre de tu vista principal
+        else:
+            messages.error(request, 'Usuario o contraseña incorrectos')
     return render(request, 'login.html')
 
 def index(request):
