3fbb39f8ddca41a3902f121f5abf9dcd5da5cfa2
Carlos Fernando Muñoz <ferxnanx1094@gmail.com>
2025-06-08 13:52:48 -0600

    Regresando a la ultima version

--- JyGDreams/templatetags/dict_get.py
+++
@@ -1,6 +0,0 @@
-from django import template
-register = template.Library()
-
-@register.filter
-def dict_get(d, k):
-    return d.get(k)
--- secret_key.py
+++
@@ -1,8 +0,0 @@
-import secrets
-import string
-
-def generate_secret_key():
-    chars = string.ascii_letters + string.digits + '!@#$%^&*(-_=+)'
-    return ''.join(secrets.choice(chars) for _ in range(50))
-
-print(generate_secret_key())
